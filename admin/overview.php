f7914fc9009babf0e567739beee04168d1460ce4
Evan <contact@evandev.fr>
2023-04-11 15:18:40 +0200

    add file systeme for admin blog

--- admin/overview.php
+++ admin/overview.php
@@ -3,37 +3,54 @@ session_start();
 require("../conf/conf_site.php");
 require('../modules/verif_login.php');
 
-if (isset($_POST['editor'])) {
-
+if (!empty($_POST['editor']) && !empty($_FILES['image'])) {
   $content = $_POST['editor'];
-
-  $article = $bdd->prepare("INSERT INTO articles(contenue) VALUES (?)");
-  $article->execute([$content]);
+  $allowed_types = array('jpg', 'jpeg', 'png', 'gif');
+  $file_extension = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
+  if (in_array($file_extension, $allowed_types)) {
+    $upload_dir = '../public/img/';
+    $filename = uniqid() . '.' . $file_extension;
+    $upload_path = $upload_dir . $filename;
+    if (move_uploaded_file($_FILES['image']['tmp_name'], $upload_path)) {
+      $article = $bdd->prepare("INSERT INTO articles(contenue,img) VALUES (?, ?)");
+      $article->execute(array($content, $filename));
+      echo 'Le fichier a été uploadé avec succès.';
+    } else {
+      echo 'Une erreur est survenue lors de l\'upload du fichier.';
+    }
+  } else {
+    echo 'Le fichier doit être une image (jpg, jpeg, png ou gif).';
+  }
 }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>Admin Page</title>
-    <script src="https://cdn.ckeditor.com/ckeditor5/11.0.1/classic/ckeditor.js"></script>
-</head>
-<body>
-    <h1>Ajouter un article</h1>
 
-    <form method="POST" action="">
-      <textarea name="editor" id="editor">
-        
-      </textarea>
+<head>
+  <meta charset="utf-8">
+  <title>Admin Page</title>
+  <link rel="stylesheet" href="../asset/css/main.css">
+  <script src="https://cdn.ckeditor.com/ckeditor5/11.0.1/classic/ckeditor.js"></script>
+</head>
+
+<body>
+  <h2 style="text-align: center;">Ajouter un article</h2>
+  <a href="../logout.php">Se déconnecter</a>
+  <div class="container-overview">
+    <form method="POST" enctype="multipart/form-data">
+      <textarea name="editor" id="editor"></textarea>
+      <input type="file" name="image" placeholder="Image">
       <button type="submit">Nouveau Poste</button>
     </form>
-
-    <script>
-        ClassicEditor
-            .create( document.querySelector( '#editor' ) )
-            .catch( error => {
-                console.error( error );
-            } );
-    </script>
+  </div>
+  <script>
+    ClassicEditor
+      .create(document.querySelector('#editor'))
+      .catch(error => {
+        console.error(error);
+      });
+  </script>
 </body>
+
 </html>
