441e51ef20668b2fedd621a87bcfe6e6397d26fe
Evan <contact@evandev.fr>
2023-04-11 09:33:15 +0200

    init login / register

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php 
+require("./conf/conf.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form method="post">
+        <input type="text" name="user" id="">
+        <input type="password" name="mdp" id="">
+        <button type="submit">Send</button>
+    </form>
+</body>
+</html>
