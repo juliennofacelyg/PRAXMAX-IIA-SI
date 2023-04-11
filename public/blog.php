afec603324dc43e20f8c247e8946a018ab117246
Evan <contact@evandev.fr>
2023-04-11 15:19:01 +0200

    edit & add blog image

--- public/blog.php
+++ public/blog.php
@@ -19,24 +19,30 @@ $articles = $articleslist->fetchAll(PDO::FETCH_ASSOC);
 <body>
     <ul class="navbar">
         <li><a href="#home">Blog.com</a></li>
-        <li style="float:right"><a class="active" href="login.php">Login</a></li>
+        <li style="float:right"><a class="active" href="../login.php">Login</a></li>
     </ul>
 
 
 
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Tous les articles</title>
-</head>
-<body>
-    <h1>Tous les articles</h1>
-    <?php foreach ($articles as $article): ?>
-        <div class="container-blog"><?php echo$article['contenue']; ?></div>
-    <?php endforeach; ?>
-</body>
-</html>
+    <!DOCTYPE html>
+    <html lang="fr">
+
+    <head>
+        <meta charset="UTF-8">
+        <title>Tous les articles</title>
+    </head>
+
+    <body>
+        <h1>Tous les articles</h1>
+        <?php foreach ($articles as $article) : ?>
+            <div class="container-blog">
+                <?php echo $article['contenue']; ?>
+                <img src="img/<?= $article['img'] ?>" alt="<?= $article['contenue']?>">
+            </div>
+        <?php endforeach; ?>
+    </body>
+
+    </html>
 
 </body>
 
