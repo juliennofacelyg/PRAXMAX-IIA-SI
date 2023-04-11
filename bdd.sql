4f2f41c9611f2c84fa313fcf3b8d43fdbc1799fd
Evan <contact@evandev.fr>
2023-04-11 13:41:01 +0200

    update sql

--- bdd.sql
+++ bdd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 11 avr. 2023 à 08:40
+-- Généré le : mar. 11 avr. 2023 à 11:39
 -- Version du serveur : 8.0.31
 -- Version de PHP : 8.0.26
 
@@ -30,19 +30,22 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `adresse_mail` varchar(50) NOT NULL,
-  `nom` varchar(25) NOT NULL,
-  `prenom` varchar(25) NOT NULL,
+  `adresse_mail` varchar(200) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `nom` varchar(90) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `prenom` varchar(90) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
   `mdp` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+  `creation` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `derniere_connexion` date NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `adresse_mail` (`adresse_mail`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3;
 
 --
 -- Déchargement des données de la table `users`
 --
 
-INSERT INTO `users` (`id`, `adresse_mail`, `nom`, `prenom`, `mdp`) VALUES
-(1, 'contact@admin.com', 'admin', 'admin', '057ba03d6c44104863dc7361fe4578965d1887360f90a0895882e58a6248fc86');
+INSERT INTO `users` (`id`, `adresse_mail`, `nom`, `prenom`, `mdp`, `creation`, `derniere_connexion`) VALUES
+(1, 'contact@admin.com', 'admin', 'admin', '057ba03d6c44104863dc7361fe4578965d1887360f90a0895882e58a6248fc86', '2023-04-11 12:25:13', '0000-00-00');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
